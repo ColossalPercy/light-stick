46fe6fdd873f8ebe35662c44bafa63fe60e59c41
jackspercy <jackspercy@gmail.com>
2019-01-21 19:53:13 +0000

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-app dark>
-      <v-toolbar fixed>
+      <v-toolbar fixed app>
         <v-toolbar-title>Light Stick</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
@@ -13,23 +13,38 @@
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <!-- <nav
-        class="v-toolbar v-toolbar--fixed theme--dark"
-        data-booted="true"
-        style="margin-top: 0px; padding-right: 0px; padding-left: 0px; transform: translateY(0px);"
-      >
-        <div class="v-toolbar__content" style="height: 64px;">
-          <div class="v-toolbar__title headline text-uppercase">
-            <span>Light Stick</span>
-          </div>
-        </div>
-      </nav>-->
+
+      <v-content>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-bottom-nav :active.sync="activeView" fixed mandatory :value="true" color="blue">
+        <v-btn flat value="gradient">
+          <span>Gradient</span>
+          <v-icon>gradient</v-icon>
+        </v-btn>
+        <v-btn flat value="presets">
+          <span>Presets</span>
+          <v-icon>save</v-icon>
+        </v-btn>
+        <v-btn flat value="image">
+          <span>Image</span>
+          <v-icon>photo</v-icon>
+        </v-btn>
+      </v-bottom-nav>
     </v-app>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeView: 'presets',
+      tabOrder: ['gradient', 'presets', 'image']
+    }
+  },
   computed: {
     ledState () {
       return this.$store.state.ledState
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
 import './assets/MI-Reg.css'
 import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
